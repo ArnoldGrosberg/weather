370bd6c8508bb8d0a4058fb948f51a3c2f8ea04b
Arnold Grosberg <arnold.grosberg@khk.ee>
2021-11-08 14:26:34 +0200

    Weather URL change

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ document.addEventListener('DOMContentLoaded', cityWeather)
 
 function weatherDataFetch(city){
 	var key = '10ed3bdf99d81facf362c6aa87f1ef4e'
-	fetch(`https://openweathermap.org/data/2.5/weather?q=${city}&appid=${key}`)
+	fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${key}`)
 	.then(function(resp){
 		return resp.json()
 	}) // Convert data to json
